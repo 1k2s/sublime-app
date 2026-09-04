2e806e82514097a7c288706cca7e86010fb81479
VictorLemosbar <victor.escolas9045@gmail.com>
2026-09-04 20:57:49 -0300

    Feat - Model finalizado (com placeholder na pk)

--- src/main/java/br/com/senai/sublime_app/provider/model/ProviderModel.java
+++ src/main/java/br/com/senai/sublime_app/provider/model/ProviderModel.java
@@ -1,12 +1,16 @@
 package br.com.senai.sublime_app.provider.model;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,29 +18,30 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-// bigint id PK
-//     bigint userId FK
-//     string name
-//     decimal commissionPercentage
-//     boolean active
-
 @Entity
-@NoArgsConstructor 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
 @ToString
 public class ProviderModel {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
 
-    //PlaceHolder for the name of the provider
+    // PlaceHolder for the name of the provider
     private String userId;
-
-    
+        
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal commissionPercentage;
+
+    @Column(nullable = false)
+    private boolean active;
 }
